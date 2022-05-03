083fa8194f2dc4e61611290632cd873412b7623f
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-03 21:32:17 +0000

    Create eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let bool = true;
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      bool = false;
+  }
+  return bool;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => 
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

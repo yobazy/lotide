66148309b1372fd2929f1aa9ba87437ef47c7d5c
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-04 06:22:30 +0000

    Update output of assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,3 +6,18 @@ const eqArrays = function(arr1, arr2) {
   }
   return bool;
 };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(arr1,arr2) {
+  let actual = eqArrays(arr1,arr2)
+  assertEqual(actual, true)
+}
+
+assertArraysEqual([1,2],[1,2])

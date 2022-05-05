699ce0d698a9731447ae50272bfe663702d5c7a7
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-05 22:33:19 +0000

    Create findKey function

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object1, callback) {
+  for (let key in object1) {
+    if (callback(object1[key]) === true) {
+      return key
+    }
+  }
+  return 'undefined';
+}
+
+const output = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+console.log(output)
+
+assertEqual(output, 'noma')

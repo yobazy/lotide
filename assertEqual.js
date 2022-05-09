a5e89efd6eae51092ac26e9ad6dbe1584297b71a
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-09 04:27:58 +0000

    add test cases and refactor functions

--- assertEqual.js
+++ assertEqual.js
@@ -7,11 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual('Banana', 'Banana');
-assertEqual('Apple', 'Banana');
-assertEqual(5,5);
-assertEqual(5,4);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual('Banana', 'Banana');
+assertEqual('Apple', 'Banana');
+assertEqual(5,5);
+assertEqual(5,4);

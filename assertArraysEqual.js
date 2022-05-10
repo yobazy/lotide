3ec5f2699ab074c9a0d38efb0f4b48cf9ff2d0e9
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-10 16:03:08 +0000

    assert arrays equal refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,9 @@
-const eqArrays = function(arr1, arr2) {
-  let bool = true;
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i])
-      bool = false;
-  }
-  return bool;
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays')
+const assertEqual = require('./assertEqual')
 
 const assertArraysEqual = function(arr1,arr2) {
   let actual = eqArrays(arr1,arr2)
   assertEqual(actual, true)
 }
 
-assertArraysEqual([1,2],[1,2])
+module.exports = assertArraysEqual;
